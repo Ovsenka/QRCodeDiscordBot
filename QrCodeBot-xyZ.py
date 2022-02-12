1eb3cd032a939b502b58dc10bf68170c4efe384f
Ovsenka <ovsenka3@gmail.com>
2022-02-12 22:52:40 +0300

    Create

---
+++ QrCodeBot-xyZ.py
@@ -0,0 +1,60 @@
+import uuid
+import os
+import discord
+from SecretStorage import *
+from QRCodeBot import *
+from datetime import datetime
+
+now = datetime.now()
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('\nWe are logged in as {0.user}'.format(client))
+
+@client.event
+# Listen for an incomming message
+async def on_message(message):
+    # If the author is the robot itself, then do nothing!
+    if message.author == client.user:
+        return
+    # If the user writes $qr
+    if message.content == "$qr":
+        current_time = now.strftime("%H:%M:%S")
+        print('\n')
+        # This for loop check for all the user's DiscordID in the Database
+        if str(message.author.id) in ScholarsDict:
+            print("This user received his QR Code : " + message.author.name)
+            print("Discord ID : " + str(message.author.id))
+            print("Current time : ", current_time)
+            # value with discordID
+            botPlaceHolders = ScholarsDict[str(message.author.id)]
+            # discordID's privateKey from the database
+            accountPrivateKey = botPlaceHolders[2]
+            # discordID's EthWalletAddress from the database
+            accountAddress = botPlaceHolders[1]
+            # Get a message from AxieInfinty
+            rawMessage = getRawMessage()
+            # Sign that message with accountPrivateKey
+            signedMessage = getSignMessage(rawMessage, accountPrivateKey)
+            # Get an accessToken by submitting the signature to AxieInfinty
+            accessToken = submitSignature(signedMessage, rawMessage, accountAddress)
+            # Create a QrCode with that accessToken
+            qrCodePath = f"QRCode_{message.author.id}_{str(uuid.uuid4())[0:8]}.png"
+            qrcode.make(accessToken).save(qrCodePath)
+            # Send the QrCode the the user who asked for
+            await message.author.send(
+                "------------------------------------------------\n\n\nHello " + message.author.name + "\nHere is your new QR Code to login : ")
+            await message.author.send(file=discord.File(qrCodePath))
+            os.remove(qrCodePath)
+            return
+        else:
+            print("This user didn't receive a QR Code : " + message.author.name)
+            print("Discord ID : " + str(message.author.id))
+            print("Current time : ", current_time)
+            return
+
+print('\nDiscord QR Code Bot')
+
+#Run the client (This runs first)
+client.run(DiscordBotToken)
---
+++ README.md
@@ -0,0 +1,6 @@
+# QrCodeBot-xyZ
+- This is a Discord bot that will provide a QR Code to your Axie Infinity scholars.
+- This version of the bot is compatible with ronin!
+
+- If you want to use it with Ethereum information instead
+In submitSignaure function, change "mainnet":"ronin" to "mainnet":"ethereum"
